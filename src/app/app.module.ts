d1af7dd577512644677c2f6cb40388d8375c5ab3
matias6946 <blondoo_m@hotmail.com>
2023-08-08 15:22:47 -0300

    commit 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './comonentes/encabezado/encabezado.component';
+import { IntroComponent } from './comonentes/intro/intro.component';
+import { ServiciosComponent } from './comonentes/servicios/servicios.component';
+import { ContactoComponent } from './comonentes/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    IntroComponent,
+    ServiciosComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule
